72a3d16dc68df304d6484596295fd2b8508f80ba
Ryan <rt.kellar@gmail.com>
2025-04-13 11:50:41 -0500

    Migrated indstudy projects to submodules

--- IndependentStudy/IndependentStudySem2/test/test_postdec.h
+++
@@ -1,9 +0,0 @@
-// test_postdec.h
-#ifndef TEST_POSTDEC_H
-#define TEST_POSTDEC_H
-
-void test_postdecrement_int(void);
-void test_postdecrement_float(void);
-void test_postdecrement_double(void);
-
-#endif // TEST_POSTDEC_H
--- IndependentStudy/IndependentStudySem2/test/test_postinc.h
+++
@@ -1,9 +0,0 @@
-// test_postinc.h
-#ifndef TEST_POSTINC_H
-#define TEST_POSTINC_H
-
-void test_postincrement_int(void);
-void test_postincrement_float(void);
-void test_postincrement_double(void);
-
-#endif // TEST_POSTINC_H
