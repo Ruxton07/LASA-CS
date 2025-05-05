ffcdaa5198218fdaf534ac187bfdc1e1132c1520
Ryan <rt.kellar@gmail.com>
2025-05-05 13:53:42 -0500

    Updated LALU and add armasm

---
+++ DigitalElectronics/ARMInstructions/MaxOf3.s
@@ -0,0 +1,11 @@
+DAT    DCD     0x45,0xc1,0x1f
+       LDR     R0, =0x200 ; base address
+       LDR     R1, [R0], #4 ; R1 = P
+       LDR     R2, [R0], #4 ; R2 = Q
+       LDR     R3, [R0] ; R3 = R
+
+       CMP     R2, R1 ; Compare Q to P
+       MOVGT   R1, R2 ; If Q > P, R1 = Q
+
+       CMP     R3, R1 ; Compare R to current max
+       MOVGT   R1, R3 ; If R > max, R1 = R
